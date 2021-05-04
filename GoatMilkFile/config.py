f412813b40f966f349294ab440d1905d8c84744f
ablocker <oct-month@qq.com>
2021-05-04 17:10:17 +0800

    'bug'

--- GoatMilkFile/config.py
+++ GoatMilkFile/config.py
@@ -2,6 +2,6 @@ import os
 
 mysql_host = os.getenv('MYSQL_HOST', 'goat-mysql')
 
-SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://goat:goat@{mysql_host}:3306/GoatMilk?charset=utf8"
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:10101010@{mysql_host}:3306/GoatMilk?charset=utf8"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
--- mysql/run.sh
+++ mysql/run.sh
@@ -4,6 +4,7 @@ docker run \
     -p 3306:3306 \
     -v /root/GoatMilk/mysql/data:/var/lib/mysql \
     -e MYSQL_ROOT_PASSWORD=10101010 \
+    -e MYSQL_DATABASE=GoatMilk \
     -d \
     mysql:8 \
     --character-set-server=utf8mb4 \
