587707855fc6eeb033e46a092c8008fc301cd978
ablocker <oct-month@qq.com>
2021-05-04 18:17:55 +0800

    'bug'

--- pre.sh
+++ pre.sh
@@ -2,7 +2,7 @@ export WORKDIR=$(pwd)
 
 mkdir $WORKDIR/mysql/sql
 mkdir $WORKDIR/GoatMilkFile/static
-rm -rf $WORKDIR/mysql/data
+#rm -rf $WORKDIR/mysql/data
 cp $WORKDIR/GoatMilkPowder/src/main/resources/schema.sql $WORKDIR/mysql/sql/a.sql
 cp $WORKDIR/GoatMilkPowder/src/main/resources/data.sql $WORKDIR/mysql/sql/b.sql
 
