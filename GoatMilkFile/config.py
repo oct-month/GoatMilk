a787c919147c229c8853d517c7adce04c19da0ad
ablocker <oct-month@qq.com>
2021-07-30 11:42:17 +0800

    bug

--- GoatMilkFile/config.py
+++ GoatMilkFile/config.py
@@ -2,6 +2,6 @@ import os
 
 postgresql_host = os.getenv('POSTGRESQL_HOST', 'goat-postgresql')
 
-SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://postgres:10101010@{postgresql_host}:5432/GoatMilk?charset=utf8"
+SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://postgres:10101010@{postgresql_host}:5432/GoatMilk"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
