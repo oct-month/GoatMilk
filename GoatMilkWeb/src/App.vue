3a42622e16a7d711881b03656b3b89437ac7cfb9
ablocker <oct-month@qq.com>
2021-10-16 18:13:33 +0800

    '登录问题'

--- GoatMilkWeb/src/App.vue
+++ GoatMilkWeb/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-      <router-view></router-view>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -10,27 +10,50 @@
 }
 
 .el-header {
-  background-color: #B3C0D1;
+  background-color: #b3c0d1;
   color: #333;
   /* line-height: 60px; */
 }
 
 .el-aside {
-  color: #B3c0D1;
+  color: #b3c0d1;
 }
 </style>
 
 <script>
-  export default {
-    data() {
-      const item = {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      };
-      return {
-        tableData: Array(20).fill(item)
-      }
-    }
-  };
+import axios from "axios";
+axios.defaults.withCredentials = true;
+
+export default {
+  data() {
+    const item = {
+      date: "2016-05-02",
+      name: "王小虎",
+      address: "上海市普陀区金沙江路 1518 弄",
+    };
+    return {
+      tableData: Array(20).fill(item),
+    };
+  },
+  methods: {
+    mounted() {
+      var that = this;
+      axios
+        .get(process.env.VUE_APP_URL + "/api/account/islogin")
+        .then((res) => {
+          if (res.data.status === "success") {
+            that.$store.commit({
+              type: "login",
+              username: res.data.data.username,
+              userrole: res.data.data.role,
+            });
+          } else {
+            that.$router.replace({ path: "/login" });
+            // console.log(this.$store.state.user_role) // 拿到用户的role
+          }
+        })
+        .catch((error) => {});
+    },
+  },
+};
 </script>
