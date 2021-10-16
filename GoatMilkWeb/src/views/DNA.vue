ead6cbcc85b5de7592f670d3ec4784952af5327f
‘mike’ <mike_s@163.com>
2021-10-16 17:30:42 +0800

    Excel

--- GoatMilkWeb/src/views/DNA.vue
+++ GoatMilkWeb/src/views/DNA.vue
@@ -97,7 +97,17 @@ export default {
         this.currentTabelData = this.tableData.slice(start, end)
       },
       exportData(){
-
+      // let table = document.getElementById('currentTabelData');
+      // let worksheet = XLSX.utils.table_to_sheet(table);
+      // let workbook = XLSX.utils.book_new();
+      // XLSX.utils.book_append_sheet(workbook, worksheet, 'sheet');
+      // 以上四行也可以直接一行搞定，如果不需要对表格数据进行修改的话
+      let workbook = XLSX.utils.table_to_book(document.getElementById("myTable"))
+      try {
+        XLSX.writeFile(workbook, 'text.xlsx');
+      } catch(e) {
+        console.log(e, workbook);
+      }
     }
   },
 
