9d9d455a22e6dcc8f05bff1aa851d258e1ceabaa
sun <ablocker@163.com>
2022-04-29 15:46:53 +0800

    docker

--- pre.sh
+++ pre.sh
@@ -1,15 +1,7 @@
 export WORKDIR=$(pwd)
 
-mkdir $WORKDIR/postgresql/sql
-mkdir $WORKDIR/GoatMilkFile/static
 #rm -rf $WORKDIR/postgresql/data
 cp $WORKDIR/GoatMilkPowder/src/main/resources/schema.sql $WORKDIR/postgresql/sql/b.sql
 cp $WORKDIR/GoatMilkPowder/src/main/resources/data.sql $WORKDIR/postgresql/sql/c.sql
-
-cd $WORKDIR/GoatMilkPowder
-sh pre.sh
-
-cd $WORKDIR/GoatMilkWeb
-sh pre.sh
 
 cd $WORKDIR
